0914203369812f0eab29526e9615d7dd5881fef8
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-14 00:42:54 -0300

    06.04 - Estrutura Condicional Switch

---
+++ 06-04-Estrutura-Condicional-Switch/EstruturaCondicionalSwitch/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace EstruturaCondicionalSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Selecione a opção:");
+            var opcao = Console.ReadLine();
+            switch(opcao)
+            {
+                case "1":
+                Console.WriteLine("Seu saldo é R$ 100,00");
+                break;
+                case "2":
+                Console.WriteLine("Informe o valor do depósito");
+                break;
+                case "3":
+                Console.WriteLine("Informe o valor do saque");
+                break;
+                default:
+                Console.WriteLine("Opção inválida");
+                break;
+            }
+        }
+    }
+}
