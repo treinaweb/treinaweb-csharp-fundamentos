4a4b74075d4ef5679b5621572067eaf900b95f00
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-14 00:38:17 -0300

    05.05 - Operadores De Comparação

---
+++ 05-05-Operadores-De-Comparacao/OperadoresComparacao/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace OperadoresComparacao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {        
+            var a = 2;
+            var b = 3;    
+            var iguais = a == b;
+            var diferentes = a != b;
+            var maior = a > b;
+            var menor = a < b;
+            var maiorOuIgual = a >= b;
+            var menorOuIgual = a <= b;
+            Console.WriteLine(menorOuIgual);
+        }
+    }
+}
