40082e36d2e559ec5d4ed2704c2c9f5fab998052
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-14 00:25:26 -0300

    04.01 - Declaração De Variáveis

---
+++ 04-01-Declaracao-De-Variaveis/DeclaracaoVariaveis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DeclaracaoVariaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;
+            decimal altura;
+            string nome;
+
+            nome = "Joel";
+
+            string email = "joel@email.com";
+
+            Console.WriteLine(email);
+        }
+    }
+}
