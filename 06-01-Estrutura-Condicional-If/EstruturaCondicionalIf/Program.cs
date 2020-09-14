ace1b150c0bb5d39ea14ee4ce02c1aea2ffb4530
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-14 00:40:27 -0300

    06.01 - Estrutura Condicional If

---
+++ 06-01-Estrutura-Condicional-If/EstruturaCondicionalIf/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace EstruturaCondicionalIf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 3;
+            var b = 2;
+            var ativo = true;
+            if(a > b && ativo == true)
+            {
+                Console.WriteLine("Condição verdadeira");
+                Console.WriteLine("É maior");
+            }
+            Console.WriteLine("Final");
+        }
+    }
+}
