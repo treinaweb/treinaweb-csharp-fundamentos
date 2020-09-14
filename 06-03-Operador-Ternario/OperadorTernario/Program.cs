ee0e7cc2f9c2905f5736e2e5a8e4e910e16c8903
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-14 00:42:00 -0300

    06.03 - Operador Ternário

---
+++ 06-03-Operador-Ternario/OperadorTernario/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace OperadorTernario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var mensagem = "";
+            var idade = 15;
+            var nota = 9;
+
+            // if(idade >= 18)
+            //     mensagem = "Maior de idade";
+            // else
+            //     mensagem = "Menor de idade";
+
+            //CONDIÇÃO ? VALOR SE VERDADEIRO : VALOR SE FALSO
+
+            // mensagem = idade >= 18 ? "Maior de idade" : "Menor de idade";
+            mensagem = nota > 7 ? "Aprovado" : "Reprovado";
+            
+            Console.WriteLine(mensagem);
+        }
+    }
+}
