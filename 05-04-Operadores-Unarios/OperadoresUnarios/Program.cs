ffa0eb40080246eb521e351aadfef5472f0a6af5
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-14 00:37:07 -0300

    05.04 - Operadores Unários

---
+++ 05-04-Operadores-Unarios/OperadoresUnarios/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OperadoresUnarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 5;
+            var b = 5;
+
+            // a++;
+            // b--;
+
+            // Console.WriteLine(a++);
+            // Console.WriteLine(b--);
+
+            Console.WriteLine(++a);
+            Console.WriteLine(--b);
+        }
+    }
+}
