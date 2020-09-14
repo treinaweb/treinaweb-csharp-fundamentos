18d4e75bf9476bc8dc237162639abc2eab0686bc
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-14 00:38:54 -0300

    05.06 - Operadores Lógicos

---
+++ 05-06-Operadores-Logicos/OperadoresLogicos/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace OperadoresLogicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ativo = false;
+            var a = 3;
+            var b = 5;
+
+            //     OPERADOR AND (E)
+            //------------------------- |
+            //OP1   | OP2   | RESULTADO |
+            //------------------------- |
+            //True  | True  | True      |
+            //True  | False | False     |
+            //False | True  | False     |
+            //False | False | False     |
+            //------------------------- |
+
+            //     OPERADOR OR (OU)
+            //------------------------- |
+            //OP1   | OP2   | RESULTADO |
+            //------------------------- |
+            //True  | True  | True      |
+            //True  | False | True      |
+            //False | True  | True      |
+            //False | False | False     |
+            //------------------------- |
+
+            Console.WriteLine(ativo == true || a > b);
+        }
+    }
+}
