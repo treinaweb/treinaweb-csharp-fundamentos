e8c903425a7b8d130d1672da8066d4a1c4b49171
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-14 01:12:13 -0300

    04.07 - Trabalhando Com Arrays

---
+++ 04-07-Trabalhando-Com-Arrays/Arrays/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numeros = new int[3];
+            numeros[0] = 10;
+            numeros[1] = 11;
+            numeros[2] = 12;
+
+            int[] pares = new int[3] { 2, 4, 6 };
+            
+            int[] impares = new int[] { 1, 3, 5, 7 };
+
+            string[] nomes = new [] { "Joao", "Pedro", "Marta" };
+
+            string[] paises = { "Brasil", "EUA", "França" };
+
+            var cidades = new [] { "São Paulo", "Rio de Janeiro" };
+
+            Console.WriteLine(cidades[0]);
+            Console.WriteLine(cidades[1]);
+        }
+    }
+}
