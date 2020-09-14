15505cf8bc34cc1c3e26ed33fead1e36ed6a3a7a
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-14 00:41:20 -0300

    06.02 - Estrutura Condicional If-Else

---
+++ 06-02-Estrutura-Condicional-If-Else/EstruturaCondicionalIfElse/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EstruturaCondicionalIfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var idade = 16;
+            var nota = 6;
+
+            if(idade >= 18)
+            {
+                Console.WriteLine("Maior de idade");
+                Console.WriteLine("Cadastro autorizado");
+            }
+            else if(idade >= 16 && idade < 18)
+            {
+                Console.WriteLine("Menor de idade (>16 anos)");
+                Console.WriteLine("Cadastro autorizado com responsáveis");
+            }
+            else
+            {
+                Console.WriteLine("Menor de idade");
+                Console.WriteLine("Cadastro não autorizado");
+            }
+
+            if(nota > 7)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+            }
+
+            Console.WriteLine("Finalizado");
+        }
+    }
+}
