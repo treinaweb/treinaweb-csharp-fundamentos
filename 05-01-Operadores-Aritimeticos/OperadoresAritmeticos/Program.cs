fe366c33b65c4904c151db383e65fba7b9edf881
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-14 00:34:06 -0300

    05.01 - Operadores Aritméticos

---
+++ 05-01-Operadores-Aritimeticos/OperadoresAritmeticos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace OperadoresAritmeticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 7;
+            var b = 3.0m;
+
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+            Console.WriteLine(1 + 2);
+            Console.WriteLine(a - 2);
+
+            var c = 10 - b;
+            Console.WriteLine(c);
+        }
+    }
+}
