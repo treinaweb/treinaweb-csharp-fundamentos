f8486ce12233f3026e6f8400b4cc86aec1af2e01
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-14 00:30:03 -0300

    04.05 - O Tipo String

---
+++ 04-05-Tipo-String/TipoString/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TipoString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            var nome = "Joel";
+            var sobrenome = "Rodrigues";
+            var nomeCompleto = nome + " " + sobrenome;
+            var nomeCompleto2 = $"{ nome } { sobrenome }.";
+
+            Console.WriteLine(nomeCompleto.ToUpper());
+            Console.WriteLine(nomeCompleto.ToLower());
+            Console.WriteLine(nomeCompleto.Substring(4));
+            Console.WriteLine(nomeCompleto.Substring(4, 5));
+            Console.WriteLine(nomeCompleto.Contains('o'));
+            Console.WriteLine(nomeCompleto.Contains("Fa"));
+            Console.WriteLine(nomeCompleto.StartsWith("J"));
+            Console.WriteLine(nomeCompleto.EndsWith("L"));
+            Console.WriteLine(nomeCompleto.IndexOf('e'));
+            Console.WriteLine(nomeCompleto.LastIndexOf('e'));
+        }
+    }
+}
