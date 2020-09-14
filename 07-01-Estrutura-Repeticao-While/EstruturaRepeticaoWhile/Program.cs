7773cb4e0fcdfdcbe645606ba00128ac378a6207
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-14 00:44:22 -0300

    07.01 - Estrutura De Repetição While

---
+++ 07-01-Estrutura-Repeticao-While/EstruturaRepeticaoWhile/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EstruturaRepeticaoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var indice = 0;
+            var versoes = new[]{ "1.0", "1.1", "2.0", "3.0" };
+
+            while(indice < versoes.Length)
+            {
+                Console.WriteLine(versoes[indice]);
+                indice++;
+            }            
+        }
+    }
+}
