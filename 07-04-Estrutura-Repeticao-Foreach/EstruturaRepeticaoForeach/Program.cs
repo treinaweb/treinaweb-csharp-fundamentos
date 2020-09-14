27992998f52d1395d02e76d1eb4a74a48b51eec2
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-14 00:46:58 -0300

    07.04 - Estrutura De Repetição Foreach

---
+++ 07-04-Estrutura-Repeticao-Foreach/EstruturaRepeticaoForeach/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EstruturaRepeticaoForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nomes = new[]{ "Joel", "Fagner", "Felipe" };
+
+            foreach(var nome in nomes)
+            {
+                Console.WriteLine(nome);
+            }
+        }
+    }
+}
