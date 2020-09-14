f12d295c64c91a1b5583c4d9ce8b0bd97061a029
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-14 00:45:52 -0300

    07.04 - Estrutura De Repetição For

---
+++ 07-03-Estrutura-Repeticao-For/EstruturaRepeticaoFor/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EstruturaRepeticaoFor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numeros = new[]{ 1, 10, 100, 1000 };
+
+            for(var indice = 0; indice < numeros.Length; indice++)
+            {
+                Console.WriteLine(numeros[indice]);
+            }
+        }
+    }
+}
