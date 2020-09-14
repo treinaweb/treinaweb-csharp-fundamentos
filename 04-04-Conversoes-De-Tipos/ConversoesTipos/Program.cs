320db6d8a4749e7f78ada61b74c9e11a708fb345
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-14 00:28:56 -0300

    04.04 - Conversões De Tipos

---
+++ 04-04-Conversoes-De-Tipos/ConversoesTipos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConversoesTipos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal taxa  = 12.98m;
+            DateTime data = DateTime.Now;
+            string taxa2 = taxa.ToString();
+            string data2 = data.ToString();
+
+            string resposta = "12";
+            int idade = int.Parse(resposta);
+            int idade2 = Convert.ToInt32(resposta);
+
+            Console.WriteLine(idade2);
+        }
+    }
+}
