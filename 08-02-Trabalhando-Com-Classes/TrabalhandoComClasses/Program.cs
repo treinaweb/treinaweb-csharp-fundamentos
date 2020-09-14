c71b37e18fdc2842e60ad860082a42b2676d1013
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-14 01:13:15 -0300

    08.02 - Trabalhando Com Classes

---
+++ 08-02-Trabalhando-Com-Classes/TrabalhandoComClasses/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TrabalhandoComClasses
+{
+    class Aluno
+    {
+        public string Nome;
+        public int Idade;
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var aluno1 = new Aluno();
+            aluno1.Nome = "Joel";
+            aluno1.Idade = 27;
+
+            var aluno2 = new Aluno();
+            aluno2.Nome = "Maria";
+            aluno2.Idade = 21;
+
+            Console.WriteLine(aluno2.Nome);
+            Console.WriteLine(aluno2.Idade);
+        }
+    }
+}
