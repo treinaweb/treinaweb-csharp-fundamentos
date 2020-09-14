3aeed56e1683e2a98cce9006bf1ea24dc2067a99
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-14 00:21:42 -0300

    03.03 - Entrada E Saída De Dados

---
+++ 03-03-Entrada-E-Saida-De-Dados/EntradaSaida/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EntradaSaida
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite seu nome:");
+            var nome = Console.ReadLine();
+            Console.WriteLine("Olá, " + nome);
+        }
+    }
+}
