b37c2056e59dc041b90e0b8a69c94b445d8ca1d0
Joel Rodrigues <joel.lima@engenhos.net>
2020-09-15 00:00:35 -0300

    10.02 - Bloco Try-Catch

---
+++ 10-02-Bloco-Try-Catch/BlocoTryCatch/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BlocoTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var nomes = new [] { "Joel", "Fagner", "Marta" };
+                for(var indice = 0; indice <= nomes.Length; indice++)
+                {
+                    Console.WriteLine(nomes[indice]);
+                }
+            }
+            catch(Exception excecao)
+            {
+                Console.WriteLine("Ocorreu um erro: " + excecao.StackTrace);
+            }
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
